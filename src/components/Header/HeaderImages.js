e8c157acc68f53dbe7f354801ff6a289d0a6ea6c
Qusai Akoud <qakoud@users.noreply.github.com>
2018-06-17 16:40:49 -0400

    Add Responsive Image component

--- src/components/Header/HeaderImages.js
+++ src/components/Header/HeaderImages.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import styles from './header-image.module.scss'
-import header1 from './header-1.jpg'
-import header2 from './header-2.jpg'
+import ResponsiveImage from '../ResponsiveImage'
 
 export default class HeaderImages extends Component {
   render() {
@@ -11,22 +10,7 @@ export default class HeaderImages extends Component {
       const header_images = doc.header_images.map((image, index) => {
         return (
           <div className={styles.header_image} key={index}>
-            <picture>
-              <source
-                media="(max-width: 400px)"
-                srcSet={image.header_image.md.url}
-              />
-              <source
-                media="(max-width: 768px)"
-                srcSet={image.header_image.lg.url}
-              />
-              <source
-                media="(max-width: 2560px)"
-                srcSet={image.header_image.xl.url}
-              />
-              <source srcSet={image.header_image.url} />
-              <img src={image.header_image.url} alt="" />
-            </picture>
+            <ResponsiveImage image={image.header_image} />
           </div>
         )
       })
