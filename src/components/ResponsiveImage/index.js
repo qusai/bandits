e5f3e659e89072659321456f50bb03d34f60d45d
Qusai Akoud <qakoud@users.noreply.github.com>
2018-06-17 16:56:19 -0400

    Make stateless component

--- src/components/ResponsiveImage/index.js
+++ src/components/ResponsiveImage/index.js
@@ -1,26 +1,26 @@
 import React, { Component } from 'react';
 
-export default class ResponsiveImage extends Component {
-  render() {
-    const { image } = this.props
-    
-    return (
-      <picture>
-        <source
-          media="(max-width: 400px)"
-          srcSet={image.md.url}
-        />
-        <source
-          media="(max-width: 768px)"
-          srcSet={image.lg.url}
-        />
-        <source
-          media="(max-width: 2560px)"
-          srcSet={image.xl.url}
-        />
-        <source srcSet={image.url} />
-        <img src={image.url} alt="" />
-      </picture>
-      )
-  }
-} 
+const ResponsiveImage = props => {
+  const { image } = props
+  
+  return (
+    <picture>
+      <source
+        media="(max-width: 400px)"
+        srcSet={image.md.url}
+      />
+      <source
+        media="(max-width: 768px)"
+        srcSet={image.lg.url}
+      />
+      <source
+        media="(max-width: 2560px)"
+        srcSet={image.xl.url}
+      />
+      <source srcSet={image.url} />
+      <img src={image.url} alt="" />
+    </picture>
+  )
+}
+
+export default ResponsiveImage
