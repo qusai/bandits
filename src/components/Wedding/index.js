8f557d1deb868d194b9e4c4b8d594155c19ff983
Qusai Akoud <qakoud@users.noreply.github.com>
2018-06-28 18:48:57 -0400

    Style fixed photo

--- src/components/Wedding/index.js
+++ src/components/Wedding/index.js
@@ -1,22 +1,26 @@
 import React, { Component } from 'react'
 import styles from './wedding.module.scss'
+import ResponsiveImage from '../ResponsiveImage'
 
 const Wedding = ({ doc }) => {
   // Get layouts
 
   const wedding = doc.data.body.map((slice, index) => {
+    const fixedPhoto = slice.primary.fixed_photo
+
     if (slice.slice_type === '2_scroll___1_fixed') {
       // Get images in this layout
-      const sliceContent = slice.items.map((image, imageIndex) => {
-        return <img src={image.scroll_photo.url} key={imageIndex} />
+      const scrollPhoto = slice.items.map((image, imageIndex) => {
+        return <ResponsiveImage image={image.scroll_photo} key={imageIndex} />
       })
       // Create an element to show images from the layout
       return (
         <div className={styles.cols_2} key={index}>
-          <div className={styles.scroll}>{sliceContent}</div>
+          <div className={styles.scroll}>{scrollPhoto}</div>
+
           {slice.primary && (
             <div className={styles.fixed}>
-              <img src={slice.primary.fixed_photo.url} />
+              <ResponsiveImage image={fixedPhoto} />
             </div>
           )}
         </div>
@@ -24,6 +28,7 @@ const Wedding = ({ doc }) => {
     }
   })
 
+  // Conditional to show only markup if there's content
   if (doc.data.body.length) {
     return <section className={styles.wedding}>{wedding}</section>
   } else {
