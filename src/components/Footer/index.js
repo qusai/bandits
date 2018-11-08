704b4b79716859432588cc64a7c995bd921d4976
Qusai Akoud <qakoud@users.noreply.github.com>
2018-11-07 21:43:00 -0500

    add contact section

---
+++ src/components/Footer/index.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import styles from './footer.module.scss'
+
+const Footer = () => {
+  const date = new Date()
+
+  return (
+    <footer className={styles.footer}>
+      <ul className={styles.footer_contact_info}>
+        <li>
+          <a href="mailto:hello@bandits.photography" title="Send us an email">
+            Email
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.instagram.com/bandits.photography/"
+            target="_blank"
+            title="Follow Bandits Photography on Instagram"
+          >
+            Instagram
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://twitter.com/BanditsWedding"
+            target="_blank"
+            title="Follow Bandits Photography on Twitter"
+          >
+            Twitter
+          </a>
+        </li>
+      </ul>
+      <p className={styles.copyrights}>©{date.getFullYear()}</p>
+    </footer>
+  )
+}
+
+export default Footer
