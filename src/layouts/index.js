942b7f348cd8fd500ebd88a6c9e054b90ac35dbb
Qusai Akoud <qakoud@users.noreply.github.com>
2018-12-21 13:12:43 -0500

    add site title/desc

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -4,7 +4,19 @@ import Helmet from 'react-helmet'
 
 import './global.scss'
 
-const Layout = ({ children, data }) => <main>{children()}</main>
+const Layout = ({ children, data }) => (
+  <main>
+    <Helmet>
+      <title>Bandits Wedding Photography</title>
+      <meta
+        name="description"
+        content="Bandits is an artist duo, wedding photographers based in Boston MA. We work with select couples to capture their love story."
+      />
+      <meta name="author" content="Qusai Akoud" />
+    </Helmet>
+    {children()}
+  </main>
+)
 
 Layout.propTypes = {
   children: PropTypes.func,
