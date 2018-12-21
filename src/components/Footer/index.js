6b355cf887474cb3c1239f23a2e850bc05d92632
Qusai Akoud <qakoud@users.noreply.github.com>
2018-12-21 15:52:37 -0500

    styles footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -6,31 +6,38 @@ const Footer = () => {
 
   return (
     <footer id="contact" className={styles.footer}>
-      <ul className={styles.footer_contact_info}>
-        <li>
+      <h3 className={styles.footer_title}>Let's talk</h3>
+      <div className={styles.footer_contact_info}>
+        <div className={styles.footer_social}>
+          <div className={styles.footer_contact}>
+            <a
+              href="https://www.instagram.com/bandits.photography/"
+              target="_blank"
+              title="Follow Bandits Photography on Instagram"
+            >
+              Instagram
+            </a>
+          </div>
+
+          <div className={styles.footer_ornament} />
+
+          <div className={styles.footer_contact}>
+            <a
+              href="https://twitter.com/BanditsWedding"
+              target="_blank"
+              title="Follow Bandits Photography on Twitter"
+            >
+              Twitter
+            </a>
+          </div>
+        </div>
+
+        <div className={styles.footer_contact}>
           <a href="mailto:hello@bandits.photography" title="Send us an email">
             Email Us
           </a>
-        </li>
-        <li>
-          <a
-            href="https://www.instagram.com/bandits.photography/"
-            target="_blank"
-            title="Follow Bandits Photography on Instagram"
-          >
-            Instagram
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://twitter.com/BanditsWedding"
-            target="_blank"
-            title="Follow Bandits Photography on Twitter"
-          >
-            Twitter
-          </a>
-        </li>
-      </ul>
+        </div>
+      </div>
       <p className={styles.copyrights}>
         ©{date.getFullYear()}. <a href="/">Bandits.</a>
       </p>
